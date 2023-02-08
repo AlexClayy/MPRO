841dbdd9fa84d4aaa1b7cfd71c5dde58fc10a6e1
AlexClayy <96576445+AlexClayy@users.noreply.github.com>
2023-02-08 08:41:30 +0000

    Delete src/pages/components directory

--- src/pages/components/api.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-export async function getBalanceByAddress(chainAddress) {
-    const resp = await axios.get("https://api.goby.app/v1/balance", { params: { address: chainAddress } });
-    return resp.data.amount;
-}
